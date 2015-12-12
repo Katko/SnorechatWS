43e552c514cdf2c8334205c35107a80addf5f466
ChaliasosKonstantinos <chaliasos@gmail.com>
2015-12-13 00:31:50 +0200

    Message parser #21

--- src/main/java/com/WebServices/SnoreChatWebServices.java
+++ src/main/java/com/WebServices/SnoreChatWebServices.java
@@ -5,13 +5,15 @@
  */
 package com.WebServices;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import static java.lang.Compiler.command;
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
@@ -25,22 +27,26 @@ public class SnoreChatWebServices {
 
     /**
      * This is a sample web service operation
+     * @param txt
      */
     @WebMethod(operationName = "chatOperation")
-    public void chatOperation(@WebParam(name = "message") String txt) {
+    public void chatOperation(@WebParam(name = "message") String txt){
        
         
         Path p = Paths.get("C://SnoreChatFiles/writeToFile.txt");
         String fileName = p.toString();
-        try{
-            PrintWriter outputStream = new PrintWriter(fileName);
-            outputStream.println(txt);
-            outputStream.close();
-        }catch(FileNotFoundException ex){
-            ex.printStackTrace();
-            
-        }
         
-    
-    }    
+        try {
+            Writer writer = new BufferedWriter(new OutputStreamWriter(
+                new FileOutputStream(fileName, true), "UTF-8")); 
+            writer.append(txt);
+            writer.append(":");
+            writer.append(txt);
+            writer.append(System.getProperty("line.separator"));
+            writer.close();
+        } catch (IOException ex) {
+            Logger.getLogger(SnoreChatWebServices.class.getName()).log(Level.SEVERE, null, ex);
+        } 
+        
+    }
 }
