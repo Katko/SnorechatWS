50d48ae6d3bc2ac79e3a552c1cbfed874e2a2a5f
michtsim <mikenight200@gmail.com>
2015-12-17 12:48:48 +0200

    Read From File Issue #25 @Katko

--- src/main/java/com/WebServices/SnoreChatWebServices.java
+++ src/main/java/com/WebServices/SnoreChatWebServices.java
@@ -5,13 +5,18 @@
  */
 package com.WebServices;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.jws.WebService;
@@ -29,16 +34,15 @@ public class SnoreChatWebServices {
      * This is a sample web service operation
      * @param txt
      */
+    // Methodos gia thn litourgia tou chat
     @WebMethod(operationName = "chatOperation")
     public void chatOperation(@WebParam(name = "message") String txt){
-       
-        
         Path p = Paths.get("C://SnoreChatFiles/writeToFile.txt");
         String fileName = p.toString();
         
         try {
             Writer writer = new BufferedWriter(new OutputStreamWriter(
-                new FileOutputStream(fileName, true), "UTF-8")); 
+            new FileOutputStream(fileName, true), "UTF-8")); 
             writer.append(txt);
             writer.append(":");
             writer.append(txt);
@@ -49,4 +53,23 @@ public class SnoreChatWebServices {
         } 
         
     }
+    //Methodos poy diavazei to arxeio
+     @WebMethod(operationName = "readFileOperation")
+    public List<String> readFileOperation() throws FileNotFoundException, IOException{
+            String line;
+            List<String> mArchive = new ArrayList<String>(); 
+            Path p = Paths.get("C://SnoreChatFiles/writeToFile.txt");
+            String fileName = p.toString();
+            BufferedReader input = new BufferedReader(new FileReader(fileName));
+        
+            while((line = input.readLine()) != null ){
+            mArchive.add(line);
+        }
+        input.close();
+        return mArchive;
+        
+    
+    }
+    
+    
 }
