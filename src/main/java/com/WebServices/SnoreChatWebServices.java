0fa7988f7ea29370dcbe6711722b68d9bb1f1d92
ChaliasosKonstantinos <chaliasos@gmail.com>
2015-12-20 20:44:36 +0200

    ADDED: #26 Username is parsing along with the message
    FIXED:
    Receiver encoding to UTF-8

--- src/main/java/com/WebServices/SnoreChatWebServices.java
+++ src/main/java/com/WebServices/SnoreChatWebServices.java
@@ -5,13 +5,20 @@
  */
 package com.WebServices;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.jws.WebService;
@@ -28,9 +35,11 @@ public class SnoreChatWebServices {
     /**
      * This is a sample web service operation
      * @param txt
+     * @param username
      */
     @WebMethod(operationName = "chatOperation")
-    public void chatOperation(@WebParam(name = "message") String txt){
+    public void chatOperation(@WebParam(name = "username") String username,
+            @WebParam(name = "message") String message){
        
         
         Path p = Paths.get("C://SnoreChatFiles/writeToFile.txt");
@@ -39,14 +48,35 @@ public class SnoreChatWebServices {
         try {
             Writer writer = new BufferedWriter(new OutputStreamWriter(
                 new FileOutputStream(fileName, true), "UTF-8")); 
-            writer.append(txt);
-            writer.append(":");
-            writer.append(txt);
+            writer.append(username);
+            writer.append(":  ");
+            writer.append(message);
             writer.append(System.getProperty("line.separator"));
             writer.close();
         } catch (IOException ex) {
             Logger.getLogger(SnoreChatWebServices.class.getName()).log(Level.SEVERE, null, ex);
         } 
         
+    }
+    
+    //Reads the File and returns the chat content
+     @WebMethod(operationName = "readFileOperation")
+    public List<String> readFileOperation() throws FileNotFoundException, IOException{
+            
+            String line;
+            List<String> mArchive = new ArrayList<String>(); 
+            Path p = Paths.get("C://SnoreChatFiles/writeToFile.txt");
+            String fileName = p.toString();
+            
+            BufferedReader input = new BufferedReader(
+           new InputStreamReader(new FileInputStream(fileName), "UTF8"));
+        
+            while((line = input.readLine()) != null ){
+            mArchive.add(line);
+        }
+        input.close();
+        return mArchive;
+        
+    
     }
 }
